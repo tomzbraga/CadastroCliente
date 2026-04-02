2675f4a2f298cdbdf98a3f3e6d2274ccf7a3b0f3
Lucas Tomaz <tomzbraga@gmail.com>
2026-04-02 19:07:59 -0300

    Adicionando a API em Java

---
+++ API/cadastro-cliente/src/main/java/com/cadastrocliente/cadastro_cliente/CadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrocliente.cadastro_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteApplication.class, args);
+	}
+
+}
---
+++ API/cadastro-cliente/src/test/java/com/cadastrocliente/cadastro_cliente/CadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrocliente.cadastro_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
